621d24e17df650ddadff65e1625d8134a440476e
Alexander Duong <Alexander.Duong@yahoo.com>
2016-05-08 17:10:00 -0700

    Update Main.java
    This is basically a demo of our program right now. It takes in a file, encodes it, and then decodes it.

--- Main.java
+++ Main.java
@@ -3,9 +3,12 @@ package cs146;
 public class Main {
 
     public static void main(String[] args) {
-	    HuffmanCoder test = new HuffmanCoder(args[0]);
+	String filename = "args[0]";
+	HuffmanCoder test = new HuffmanCoder(filename);
         test.getQuantityHashMap();
-        test.printCountMap();
-        HuffmanCoder.makeHuffmanTree(test.countMap);
+        test.makeHuffManTree();
+        test.buildCode();
+        test.encode(filename, test.buildMap);
+        test.decode(HuffmanCoder.getHuffFilename(filename), test.decodeMap);
     }
 }
