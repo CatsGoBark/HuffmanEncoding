0819652f5b1431ed4c66e4cdad04e20cb5f2e47d
Alexander Duong <Alexander.Duong@yahoo.com>
2016-05-08 17:31:41 -0700

    Update Main.java
    Made it simpler to demo.

--- Main.java
+++ Main.java
@@ -3,12 +3,6 @@ package cs146;
 public class Main {
 
     public static void main(String[] args) {
-	String filename = "args[0]";
-	HuffmanCoder test = new HuffmanCoder(filename);
-        test.getQuantityHashMap();
-        test.makeHuffManTree();
-        test.buildCode();
-        test.encode(filename, test.buildMap);
-        test.decode(HuffmanCoder.getHuffFilename(filename), test.decodeMap);
+	HuffmanCoder.demo(args[0]);
     }
 }
